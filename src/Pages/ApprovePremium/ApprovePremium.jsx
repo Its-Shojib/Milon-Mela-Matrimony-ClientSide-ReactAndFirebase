4bd47544a06ab045dde87f1aeb44da368f36b0c6
Its_Shojib <mdshojib922@gmail.com>
2023-11-26 20:20:30 +0600

    Make Premium Successfully

--- src/Pages/ApprovePremium/ApprovePremium.jsx
+++ src/Pages/ApprovePremium/ApprovePremium.jsx
@@ -1,9 +1,75 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { Button, Card } from "@material-tailwind/react";
+import Swal from "sweetalert2";
 
 const ApprovePremium = () => {
+
+    let axiosSecure = useAxiosSecure();
+    const { data: premiumReqUser = [],refetch } = useQuery({
+        queryKey: ['premiumReqUser'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/premiumReqUser');
+            return res.data;
+        }
+    })
+
+    let HandleMakePremium = (email) => {
+        Swal.fire({
+            title: "Are you sure to make Premium?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Make Premium!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/biodata/premium/${email}`)
+                    .then(res => {
+                        if (res.data.modifiedCount > 0) {
+                            Swal.fire({
+                                title: "Updated!",
+                                text: 'Biodata Updated',
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
+
     return (
-        <div>
-            <p> Hello, I am ApprovePremium </p>
+        <div className="w-11/12 mx-auto my-10">
+            <Card className="h-full w-full">
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr className="pr-10">
+                            <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">Index</th>
+                            <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">User Name</th>
+                            <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">Email</th>
+                            <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">Bio Id</th>
+                            <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            premiumReqUser?.map((item, index) => <tr key={item?._id}>
+                                <th className="p-4 border-b border-blue-gray-50">{index + 1}</th>
+                                <td className="p-4 border-b border-blue-gray-50">
+                                    {item?.Name}
+                                </td>
+                                <td className="p-4 border-b border-blue-gray-50">{item?.Email}</td>
+                                <td className="p-4 border-b border-blue-gray-50">
+                                    {item?.Biodata_id}
+                                </td>
+                                <th className="p-4 border-b border-blue-gray-50"><Button onClick={()=>HandleMakePremium(item?.Email)}>Make Premium</Button></th>
+                            </tr>
+                            )}
+                    </tbody>
+                </table>
+            </Card>
         </div>
     )
 }
