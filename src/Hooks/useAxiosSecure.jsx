694413305443d3e5a475070f6c93277e0a6f12a8
Its_Shojib <mdshojib922@gmail.com>
2024-02-09 02:02:01 +0600

    Security Updated

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -7,24 +7,25 @@ let axiosSecure = axios.create({
     baseURL: 'https://milon-mela-server.vercel.app',
     withCredentials: true
 })
+
 const useAxiosSecure = () => {
     let navigate = useNavigate();
-    let {Logout} = useAuth();
-    axiosSecure.interceptors.request.use(function(config){
+    let { Logout } = useAuth();
+    axiosSecure.interceptors.request.use(function (config) {
         let token = localStorage.getItem('access-token');
         // console.log('request stoped by interceptors',token);
         config.headers.authorization = `Bearer ${token}`
         return config;
-    }, function(error){
+    }, function (error) {
         return Promise.reject(error);
     });
 
-    axiosSecure.interceptors.response.use((response)=>{
+    axiosSecure.interceptors.response.use((response) => {
         return response;
-    }, async(error)=>{
+    }, async (error) => {
         let status = error?.response?.status;
         // console.log('Status Code in interceptor: ', status);
-        if(status ===401 || status ===403){
+        if (status === 401 || status === 403) {
             await Logout();
             navigate('/login');
         }
